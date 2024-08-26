772df0620fe6df55e4d1a5eed820420941aa90de
Nils Nahooy <nils.nahooy@live.com>
2024-08-26 10:32:09 +0200

    Create Main scene UI

---
+++ scenes/main/main.gd
@@ -0,0 +1,19 @@
+extends Control
+
+const LEVEL_BUTTON: PackedScene = preload("res://scenes/level_button/level_button.tscn")
+
+@onready var grid: GridContainer = $MC/VB/LevelGridContainer
+
+@export var num_cols: int = 6
+
+
+func _ready() -> void:
+	_setup_grid()
+
+
+func _setup_grid() -> void:
+	var num_levels: int = LevelBuilder.get_num_levels()
+	grid.columns = num_cols
+	for l: int in range(num_levels):
+		var button: Node = LEVEL_BUTTON.instantiate()
+		grid.add_child(button)
--- singletons/level_builder.gd
+++ singletons/level_builder.gd
@@ -1,9 +1,11 @@
 extends Node
 
-var _level_data: Dictionary # level:LevelLayout
+var _level_data: Dictionary
+
 
 func _ready() -> void:
 	_load_data()
+
 
 func _load_data() -> void:
 	var file: FileAccess = FileAccess.open(Constants.LEVEL_DATA_PATH, FileAccess.READ)
@@ -12,8 +14,13 @@ func _load_data() -> void:
 		_level_data[lns] = LevelLayout.new(raw_data[lns])
 	file.close()
 
+
 func get_data_for_level(level: String) -> LevelLayout:
 	if not _level_data.has(level):
 		print("No data for level %s" % level)
 		return
 	return _level_data[level]
+
+
+func get_num_levels() -> int:
+	return _level_data.size()
